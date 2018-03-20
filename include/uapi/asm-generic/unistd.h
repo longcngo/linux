7eb006ef7d31e0f12d4981c16d4bdd9b091cb4ce
Long Ngo <lcngo@umass.edu>
2018-03-20 02:38:57 -0400

    draft1

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -732,6 +732,9 @@ __SYSCALL(__NR_pkey_alloc,    sys_pkey_alloc)
 __SYSCALL(__NR_pkey_free,     sys_pkey_free)
 #define __NR_statx 291
 __SYSCALL(__NR_statx,     sys_statx)
+/* My new Syscall */
+#define __NR_getpinfo 292
+__SYSCALL(__NR_getpinfo, sys_getpinfo)
 
 #undef __NR_syscalls
 #define __NR_syscalls 292
